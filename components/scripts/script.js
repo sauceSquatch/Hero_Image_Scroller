d35c4600041be88ef38507ff7d910f0b14ad4498
Jamie Naioti <Jamie.Naioti@razorfish.com>
2015-02-12 22:19:43 -0600

    put in gradient fade for image group, sweetened the transition on animation for imaege group. ready for image group rotation

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -11,14 +11,15 @@ $(document).ready(function()
 ///// INIT build
 reset = function() {
   console.log("start build");
-  TweenLite.to( $(".bg", ".image_group"), 0, {scale:1.15, opacity:0, transformOrigin:"50% 10%"});
+  TweenLite.to( $(".bg", ".image_group"), 0, {scale:1.15, transformOrigin:"50% 10%"});
   TweenLite.to( $(".cutout", ".image_group"), 0, {scale:1.35, transformOrigin:"50% 40%"})
 }
 
 transitionOnActiveImage = function() {
   var selectedGroup = $("div.active");
   console.log("selectedGroup: ", selectedGroup);
-  TweenLite.to( $(".bg", selectedGroup), 1, {opacity:1});
   TweenLite.to( $(".bg", selectedGroup), 4.25, {scale:1, ease:Power1.easeOut, delay:0.25});
-  TweenLite.to( $(".cutout", selectedGroup), 3.5, {scale:1, ease:Power3.easeInOut});
+  TweenLite.to( $(".cutout", selectedGroup), 3.5, {scale:1, ease:Power1.easeInOut});
+  TweenLite.to( $(".fade", selectedGroup), 1.5, {opacity:0});
+  TweenLite.to( $(".gradient_fade", selectedGroup), 3, {opacity:0, delay:2});
 }
